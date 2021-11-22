116e9eae6cbf1cf2f12548e574526c81b85e585d
Ratier David <ratierd@gmail.com>
2021-11-22 23:05:46 +0100

    Create codemod

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,28 +2,6 @@
 
 All notable changes to this project will be documented in this file.
 
-## 0.0.4
-
-### Added
-
-- Support for Angular ElementRef
-
-## 0.0.3
-
-### Added
-
-- Support for Angular animations
-
-## 0.0.2
-
-### Added
-
-- Support for organizing Angular & Vue attributes
-
-### Changed
-
-- Plugin automatically selects presets for languages
-
 ## 0.0.1
 
 ### Added
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,50 +2,36 @@ import { readdirSync, readFileSync } from "fs";
 import { join } from "path";
 import * as prettier from "prettier";
 import * as OrganizeAttributes from "./index";
-import { PrettierPluginOrganizeAttributesParserOptions } from "./index";
 
 const testFolder = join(__dirname, "tests");
-const languages = readdirSync(testFolder);
 
-languages.forEach((language) => {
-  describe(language, () => {
-    const languagePath = join(testFolder, language);
-    const tests = readdirSync(languagePath);
-    const extension = readFileSync(join(languagePath, "extension")).toString();
+describe("Codemod tests", () => {
+  const tests = readdirSync(testFolder);
+  const extension = "component.html";
 
-    tests
-      .filter((file) => file !== "extension")
-      .forEach((test) =>
-        it(test, () => {
-          const path = join(testFolder, language, test);
-          const inputPath = join(path, `input.${extension}`);
-          const expectedPath = join(path, `expected.${extension}`);
+  tests.forEach((test) =>
+    it(test, () => {
+      const path = join(testFolder, test);
+      const inputPath = join(path, `input.${extension}`);
+      const expectedPath = join(path, `expected.${extension}`);
 
-          const input = readFileSync(inputPath).toString();
-          const expected = readFileSync(expectedPath).toString();
-          const testConfig = JSON.parse(
-            readFileSync(join(path, "config.json")).toString()
-          );
+      const input = readFileSync(inputPath).toString();
+      const expected = readFileSync(expectedPath).toString();
 
-          const options: Partial<PrettierPluginOrganizeAttributesParserOptions> &
-            prettier.Options = {
-            filepath: inputPath,
-            ...testConfig,
-            plugins: [OrganizeAttributes],
-          };
-          const prettify = (code: string) => prettier.format(code, options);
+      const options: prettier.Options = {
+        filepath: inputPath,
+        plugins: [OrganizeAttributes],
+      };
+      const format = () => prettier.format(input, options);
 
-          const format = () => prettify(input);
+      const expectedError = expected.match(/Error\("(?<message>.*)"\)/)?.groups
+        ?.message;
 
-          const expectedError = expected.match(/Error\("(?<message>.*)"\)/)
-            ?.groups?.message;
-
-          if (expectedError) {
-            expect(format).toThrow(expectedError);
-          } else {
-            expect(format()).toEqual(expected);
-          }
-        })
-      );
-  });
+      if (expectedError) {
+        expect(format).toThrow(expectedError);
+      } else {
+        expect(format()).toEqual(expected);
+      }
+    })
+  );
 });
